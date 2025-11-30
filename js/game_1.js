8f6604cf7cdf78863b9c0eda2682d4536152088e
TrojanVoid Stopinski <d4rklink0ft1me@gmail.com>
2025-11-30 20:47:47 -0300

    i waste an entire day doing this
    uuhhhh do the games, quiz, and finish the credits

--- js/game_1.js
+++ js/game_1.js
@@ -2,12 +2,23 @@ const canvas = document.getElementById("gameWindow_1");
 const ctx = canvas.getContext("2d");
 
 const screen = {
-	width: 640,
-	height: 360
+	width: 800,
+	height: 480
 }
+
+/*
+	black like - #332C50
+	i love red - #46878
+	F
+	i dunno the name - #94E344
+	almost white - #E2F3E4
+*/
 
 canvas.width = screen.width;
 canvas.height = screen.height;
 
-ctx.fillStyle = "#008080";
+ctx.fillStyle = "#94E344";
 ctx.fillRect(0, 0, screen.width, screen.height);
+
+//the browser window will be made using a square
+//wont be a sprite
---
+++ js/game_2.js
@@ -0,0 +1,24 @@
+const canvas = document.getElementById("gameWindow_2");
+const ctx = canvas.getContext("2d");
+
+const screen = {
+	width: 800,
+	height: 480
+}
+
+/*
+	black like - #332C50
+	i love red - #46878
+	F
+	i dunno the name - #94E344
+	almost white - #E2F3E4
+*/
+
+canvas.width = screen.width;
+canvas.height = screen.height;
+
+ctx.fillStyle = "#94E344";
+ctx.fillRect(0, 0, screen.width, screen.height);
+
+//the browser window will be made using a square
+//wont be a sprite
